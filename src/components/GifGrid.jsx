ea2abe8ab0c7e92be51d265d3659e121843282bc
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-01 13:28:56 -0300

    optimizaciones

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,34 +1,20 @@
-import { useState, useEffect } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifElement } from "./GifElement";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([]);
-
-
-  useEffect(() => {
-    getGifs(category).then( newImages => setImages(newImages)); 
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
         <h3>{category}</h3>
 
+        {
+            isLoading && (<h2>Carregando...</h2>)
+        }
+
         <div className="GifGrid">
           {
-            // images.map(image => (
-            //   <li key={image.id}>{image.title}</li>
-            // ))
-            //! className="" assim tem que ser para o css
-            // images.map(({id, title, url}) => (
-            //   <GifElement
-            //     key = {id}
-            //     title = {title}
-            //     url = {url}
-            //   />
-            // ))
-            //! EM CASO DQ QUE TEMOS MUITAS PROPRIEDADES PODEMOS UAR O OPREADOR DE SPREAD
-            //! ASSIM NAO TENHO QUE ESPECIFICAR CADA PROPRIEDADE E PASA CADA UMA COM SEU RESPECTIVO VALOR E NOME
             images.map((image) => (
               <GifElement
                 key = {image.id}
