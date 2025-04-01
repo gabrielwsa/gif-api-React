10ebc2fb28484482d4dc86bb78fff92fcf8c4dc4
gabrielwsa <152181309+gabrielwsa@users.noreply.github.com>
2025-04-01 13:52:37 -0300

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=N0PJTNnOGJL6qadwwvKwc6sIyxK0gnix&q=${category}&limit=12`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key={API_KEY}&q=${category}&limit=12`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
 
