11d0547522ca47a1bd2b1aa31bdd14722ab3e15d
gabrielwsa <152181309+gabrielwsa@users.noreply.github.com>
2025-04-06 19:42:29 -0300

    correcao de variavel

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${category}&limit=12`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=12`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
 
